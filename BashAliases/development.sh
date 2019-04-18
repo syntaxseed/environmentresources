0a33e83284539a442bc020cee23b828269fa7f40
Sherri Wheeler <825423+syntaxseed@users.noreply.github.com>
2019-04-18 13:58:26 -0400

    Stop suspending php up commands to background.

--- BashAliases/development.sh
+++ BashAliases/development.sh
@@ -11,7 +11,7 @@ alias homeup='cd ~/Homestead/ ; vagrant up'
 # Run the built in php webserver for my local homepage.
 alias phphome='printf "\n\e[1;33mVisit at: http://localhost:8000\e[0m\n\n" ; cd ~/Development/Saturn-Projects/LocalPHP/public/ ; php -S localhost:8000'
 # Run php webserver in current directory. Silence output. Push process to background (& at end of command).
-alias phpup='printf "\n\e[1;33mVisit at: http://localhost:8000\e[0m\n\n" ; php -S localhost:8000 >/dev/null 2>&1 &'
+alias phpup='printf "\n\e[1;33mVisit at: http://localhost:8000\e[0m\n\n" ; php -S localhost:8000 >/dev/null 2>&1'
 # Run PHPLOC and PHPCPD and save output to a file.
 alias phpanalysis='{ printf "***** PHPLOC AND PHPCPD ANALYSIS *****\n\n" ; phploc . ; printf "\n----------------------------------\n\n" ; phpcpd . ; } > phpanalysis.txt ; printf "\n \e[1;32m>>> PPHLOC and PHPCPD output saved to phpanalysis.txt\n"'
 
@@ -20,7 +20,7 @@ alias phpanalysis='{ printf "***** PHPLOC AND PHPCPD ANALYSIS *****\n\n" ; phplo
 # Dump AutoLoad, Clear all cache and config cache.
 alias lalreset='composer dump-autoload; php artisan config:clear; php artisan cache:clear'
 # Run php webserver in current directory. Silence output. Push process to background (& at end of command).
-alias lalup='printf "\n\e[1;33mVisit at: http://127.0.0.1:8080\e[0m\n\n" ; php artisan serve --port=8080 >/dev/null 2>&1 &'
+alias lalup='printf "\n\e[1;33mVisit at: http://127.0.0.1:8080\e[0m\n\n" ; php artisan serve --port=8080 >/dev/null 2>&1'
 
 
 # ---- Editor/Environment/Tools -----
