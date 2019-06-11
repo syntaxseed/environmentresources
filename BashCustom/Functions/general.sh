dc5217092da2735cc239058babceba61e79330f8
syntaxseed <825423+syntaxseed@users.noreply.github.com>
2019-06-11 10:33:26 -0400

    Only allow backupenv function if on local machine.

--- BashCustom/Functions/general.sh
+++ BashCustom/Functions/general.sh
@@ -4,11 +4,17 @@
 
 # Function to run my backup environment scripts, then switch to the target backup repo so I can commit it.
 #    Optional: pass in the target directory for the backups as a parameter.
-backupenv() {
-    cd /home/sherriw/Development/Repos/Git/EnvironmentResources/BackupScripts/
-    sh backupenv.sh "$1"
-    cd /home/sherriw/Development/Repos/Git/EnvironmentBackup/
-}
+if [ "$IS_LOCAL_SHELL" == "1" ]; then
+    backupenv() {
+        if [ -d $HOME/Development/Repos/Git/EnvironmentResources/BackupScripts/ ]; then
+            cd $HOME/Development/Repos/Git/EnvironmentResources/BackupScripts/
+            sh backupenv.sh "$1"
+            cd $HOME/Development/Repos/Git/EnvironmentBackup/
+        else
+            echo "ERROR - Directory /Development/Repos/Git/EnvironmentResources/BackupScripts/ not found."
+        fi
+    }
+fi
 
 # Function to set the terminal title - used by tabs.
 function termtitle() {
