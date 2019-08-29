59f9fc25d4f6eaa54f945facbc3412a11516fb7e
syntaxseed <825423+syntaxseed@users.noreply.github.com>
2019-08-28 22:13:49 -0400

    Add the __git_ps2 to trueline prompt.

--- BashCustom/config_trueline.sh
+++ BashCustom/config_trueline.sh
@@ -1,6 +1,7 @@
 # Configure and include Trueline.
 
 TRUELINE_WORKING_DIR_SPACE_BETWEEN_PATH_SEPARATOR=false
+TRUELINE_GIT_SHOW_STATUS_NUMBERS=true
 
 declare -A TRUELINE_SYMBOLS=(
     [working_dir_separator]='/'
@@ -29,11 +30,32 @@ _trueline_mercurial_segment() {
     fi
 }
 
+_trueline_gitcustom_segment() {
+
+    local branch_icon="$(_trueline_git_remote_icon)"
+    branch_icon="${branch_icon#"${branch_icon%%[![:space:]]*}"}" # Trim leading whitespace.
+    branch_icon="${branch_icon%"${branch_icon##*[![:space:]]}"}"  # Trim trailing whitespace.
+
+    GIT_PS1_SHOWDIRTYSTATE=1
+    GIT_PS1_SHOWUNTRACKEDFILES=1
+    GIT_PS1_SHOWUPSTREAM="auto"
+    local some_content="$(__git_ps1 %s)"
+
+    if [[ -n "$some_content" ]]; then
+        local fg_color="$1"
+        local bg_color="$2"
+        local segment="$(_trueline_separator)"
+        segment+="$(_trueline_content "$fg_color" "$bg_color" normal " $branch_icon $some_content ")"
+        PS1+="$segment"
+        _last_color=$bg_color
+    fi
+}
+
 declare -a TRUELINE_SEGMENTS=(
     'user,green,special_grey'
     'bg_jobs,red,black'
     'working_dir,light_blue,cursor_grey'
-    'git,yellow,special_grey'
+    'gitcustom,yellow,special_grey'
     'mercurial,yellow,special_grey'
     'newline,black,yellow'
 )
