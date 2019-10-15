a2fa5a0e683227d90f48c0909d95ce6cb82d3ca5
syntaxseed <825423+syntaxseed@users.noreply.github.com>
2019-10-15 14:21:17 -0400

    Changes to php ini backups.

--- BackupScripts/backupphp.sh
+++ BackupScripts/backupphp.sh
@@ -9,6 +9,6 @@ dpkg -l | grep php > packages-with-php.txt
 echo "\n\033[1;34mPHP: Exported list of packages which contain 'php'."
 
 # Backup PHP.ini files:
-cp /etc/php/7.2/cli/php.ini php_CLI.ini
-cp /etc/php/7.2/apache2/php.ini php_APACHE2.ini
-echo "\033[1;34mPHP: Copied php.ini files."
+cp /etc/php/7.3/cli/conf.d/99-sherri-overrides.ini php_CLI_99-sherri-overrides.ini
+cp /etc/php/7.3/apache2/conf.d/99-sherri-overrides.ini php_APACHE2_99-sherri-overrides.ini
+echo "\033[1;34mPHP: Copied v7.3 php.ini over-ride files."
